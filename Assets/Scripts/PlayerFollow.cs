6d7c1baee8c08466d8ed95cfd2a2b76f3eaec22f
Alex Baratta <alexbaratta1@gmail.com>
2026-03-23 22:35:26 +0100

    fix: remove rotation logic from mob follow behavior

--- Assets/Scripts/PlayerFollow.cs
+++ Assets/Scripts/PlayerFollow.cs
@@ -27,9 +27,6 @@ public class PlayerFollow : MonoBehaviour
         {
             Vector3 direction = (target.position - transform.position).normalized;
             moveDirection = direction;
-
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
         }
         
     }
